6e30ecfb6a37a6138ae803cd1801f900b77b1a20
p1nh0 <p1nh0.c0d1ng@gmail.com>
2019-06-02 06:08:13 +0100

    updated synth diagram
    FbkDelay can also be sent to the reverb

--- B_Parabola/SynthDiagram.cpp
+++ B_Parabola/SynthDiagram.cpp
@@ -5,34 +5,34 @@
 #include <SerialFlash.h>
 
 // GUItool: begin automatically generated code
-AudioSynthWaveformSine   OscB;           //xy=62,27
-AudioSynthWaveformSine   OscD;           //xy=62,71
-AudioEffectEnvelope      EnvB;           //xy=194,27
-AudioEffectEnvelope      EnvD;           //xy=194,71
-AudioPlaySdWav           SampleA;        //xy=308,202
-AudioPlaySdWav           SampleB;        //xy=309,250
-AudioSynthNoiseWhite     Noise;          //xy=311,117
-AudioSynthWaveformSineModulated OscA;           //xy=331,27
-AudioSynthWaveformSineModulated OscC;           //xy=331,71
-AudioSynthWaveformDc     FilterEnvOffset;            //xy=428.75000762939453,153.75000190734863
-AudioEffectFade          FadeA;          //xy=449,195
-AudioEffectFade          FadeB;          //xy=450,243
-AudioEffectEnvelope      NoiseEnv;       //xy=460,117
-AudioEffectEnvelope      EnvA;           //xy=468,27
-AudioEffectEnvelope      EnvC;           //xy=468,71
-AudioEffectEnvelope      FilterEnv;      //xy=581,153
-AudioMixer4              SampleMix;      //xy=600,232
-AudioMixer4              OscMix;         //xy=614,78
-AudioEffectFade          SampleFade;     //xy=752,232
-AudioFilterStateVariable Lowpass;            //xy=764,84
-AudioMixer4              SendB;          //xy=1000,338
-AudioMixer4              SendA;          //xy=1001,237
-AudioEffectFreeverb      Reverb;         //xy=1135,237
-AudioEffectDelay         FbkDelay;       //xy=1135,338
-AudioMixer4              MainMix;        //xy=1371,85
-AudioFilterBiquad        Highpass;            //xy=1504,85
-AudioFilterBiquad        HighShelf;        //xy=1646.2500228881836,85.0000171661377
-AudioOutputI2S           Output;         //xy=1777.7500267028809,90.75000190734863
+AudioSynthWaveformSine   OscB;           //xy=56,27
+AudioSynthWaveformSine   OscD;           //xy=56,71
+AudioEffectEnvelope      EnvB;           //xy=188,27
+AudioEffectEnvelope      EnvD;           //xy=188,71
+AudioPlaySdWav           SampleA;        //xy=302,202
+AudioPlaySdWav           SampleB;        //xy=303,250
+AudioSynthNoiseWhite     Noise;          //xy=305,117
+AudioSynthWaveformSineModulated OscA;           //xy=325,27
+AudioSynthWaveformSineModulated OscC;           //xy=325,71
+AudioSynthWaveformDc     FilterEnvOffset; //xy=422,153
+AudioEffectFade          FadeA;          //xy=443,195
+AudioEffectFade          FadeB;          //xy=444,243
+AudioEffectEnvelope      NoiseEnv;       //xy=454,117
+AudioEffectEnvelope      EnvA;           //xy=462,27
+AudioEffectEnvelope      EnvC;           //xy=462,71
+AudioEffectEnvelope      FilterEnv;      //xy=575,153
+AudioMixer4              SampleMix;      //xy=594,232
+AudioMixer4              OscMix;         //xy=608,78
+AudioEffectFade          SampleFade;     //xy=746,232
+AudioFilterStateVariable Lowpass;        //xy=758,84
+AudioMixer4              SendB;          //xy=993,311
+AudioMixer4              SendA;          //xy=995,237
+AudioEffectFreeverb      Reverb;         //xy=1129,237
+AudioEffectDelay         FbkDelay;       //xy=1130,420
+AudioMixer4              MainMix;        //xy=1365,85
+AudioFilterBiquad        Highpass;       //xy=1498,85
+AudioFilterBiquad        HighShelf;      //xy=1640,85
+AudioOutputI2S           Output;         //xy=1771,90
 AudioConnection          patchCord1(OscB, EnvB);
 AudioConnection          patchCord2(OscD, EnvD);
 AudioConnection          patchCord3(EnvB, OscA);
@@ -62,8 +62,9 @@ AudioConnection          patchCord26(SendA, Reverb);
 AudioConnection          patchCord27(Reverb, 0, MainMix, 2);
 AudioConnection          patchCord28(FbkDelay, 0, MainMix, 3);
 AudioConnection          patchCord29(FbkDelay, 0, SendB, 3);
-AudioConnection          patchCord30(MainMix, Highpass);
-AudioConnection          patchCord31(Highpass, HighShelf);
-AudioConnection          patchCord32(HighShelf, 0, Output, 0);
-AudioControlSGTL5000     AudioShield;    //xy=1764.5000267028809,133.75000286102295
+AudioConnection          patchCord30(FbkDelay, 0, SendA, 3);
+AudioConnection          patchCord31(MainMix, Highpass);
+AudioConnection          patchCord32(Highpass, HighShelf);
+AudioConnection          patchCord33(HighShelf, 0, Output, 0);
+AudioControlSGTL5000     AudioShield;    //xy=1758,133
 // GUItool: end automatically generated code
